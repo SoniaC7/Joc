221b25f44139b0f410cb78b4b7d915211fe6775d
Sònia Carrancà <sonia.carranca01@estudiant.upf.edu>
2020-04-30 12:21:00 +0200

    First step
    Object moving and camera rotation

---
+++ Assets/Scripts/MovePlane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlane : MonoBehaviour
+{
+    public float speed = 10;
+    public GameObject camera;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    	if(camera.transform.rotation.y != 0){
+    		speed = 0;
+    	}
+
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    //public Vector3 rotationSpeed;
+    public GameObject Player;
+    public GameObject plane;
+    private float cam_y;
+    void Start()
+    {
+        
+    }
+    void comparePos(){
+    	
+
+    	if(plane.transform.position.z < -10.00){
+
+    		transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, 90, 0), Time.deltaTime * 10);
+    	}
+    }
+    void Update()
+    {
+    	comparePos();
+        
+        
+    }
+}
