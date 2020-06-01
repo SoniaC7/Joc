c8cd3e91b213f7a4bdeab520b2d8bf4c6996e134
GuillemPuente <59957213+GuillemPuente@users.noreply.github.com>
2020-06-01 22:44:35 +0200

    Added menu

---
+++ Rotation test/Assets/Scripts/menu_left_hand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_left_hand : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Menu_Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Rotation test/Assets/Scripts/move_camera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_camera : MonoBehaviour
+{
+
+    public string turn = "no";
+
+    private bool trigger_contact = false;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Tocando");
+        if (other.gameObject.CompareTag("Right"))
+        {
+            Debug.Log("Giro derecha");
+           // transform.Rotate(new Vector3(0, 45, 0));
+            //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, 90, 0), Time.deltaTime * tspeed);
+            turn = "right";
+        }
+
+        else if (other.gameObject.CompareTag("Left"))
+        {
+            Debug.Log("Giro izquierda");
+            //transform.Rotate(new Vector3(0, -45, 0));
+            //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, -90, 0), Time.deltaTime * tspeed);
+            turn = "left";
+        }
+        
+
+        if (other.gameObject.CompareTag("Shelter") && !trigger_contact)
+        {
+            Debug.Log("Stop");
+        }
+    }
+}
