e1d4b8679fcf6d00ffa7567a680ba02c1d7f32d0
Sònia Carrancà <sonia.carranca01@estudiant.upf.edu>
2020-06-01 15:15:56 +0200

    actualized

---
+++ Rotation test/Assets/move_camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_camera : MonoBehaviour
+{
+
+    public string turn = "no";
+
+    private bool trigger_contact = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 90, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -90, 0));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Tocando");
+        if (other.gameObject.CompareTag("Right"))
+        {
+            Debug.Log("Giro derecha");
+           // transform.Rotate(new Vector3(0, 45, 0));
+            //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, 90, 0), Time.deltaTime * tspeed);
+            turn = "right";
+        }
+
+        else if (other.gameObject.CompareTag("Left"))
+        {
+            Debug.Log("Giro izquierda");
+            //transform.Rotate(new Vector3(0, -45, 0));
+            //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, -90, 0), Time.deltaTime * tspeed);
+            turn = "left";
+        }
+        
+
+        if (other.gameObject.CompareTag("Shelter") && !trigger_contact)
+        {
+            Debug.Log("Stop");
+        }
+    }
+}
---
+++ Rotation test/Assets/move_terra.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_terra : MonoBehaviour
+{
+    public float speed;
+    public GameObject player;
+
+    private Vector3 direction = new Vector3(0, 0, -1);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (player.GetComponent<move_camera>().turn == "left") { turn_left(); player.GetComponent<move_camera>().turn = "no"; }
+        if (player.GetComponent<move_camera>().turn == "right") {turn_right(); player.GetComponent<move_camera>().turn = "no"; }
+    }
+
+    void turn_left()
+    {
+        transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), 90);
+        if (direction.Equals(new Vector3(0, 0, -1))) direction = new Vector3(1, 0, 0);
+        else if (direction.Equals(new Vector3(1, 0, 0))) direction = new Vector3(0, 0, 1);
+        else if (direction.Equals(new Vector3(0, 0, 1))) direction = new Vector3(-1, 0, 0);
+        else if (direction.Equals(new Vector3(-1, 0, 0))) direction = new Vector3(0, 0, -1);
+    }
+
+    void turn_right()
+    {
+        transform.RotateAround(player.transform.position, new Vector3(0, 1, 0), -90);
+        if (direction.Equals(new Vector3(1, 0, 0))) direction = new Vector3(0, 0, -1);
+        else if (direction.Equals(new Vector3(0, 0, 1))) direction = new Vector3(1, 0, 0);
+        else if (direction.Equals(new Vector3(-1, 0, 0))) direction = new Vector3(0, 0, 1);
+        else if (direction.Equals(new Vector3(0, 0, -1))) direction = new Vector3(-1, 0, 0);
+    }
+}
