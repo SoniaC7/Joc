daf0a49cdde6d90ccfb80983f24ca756e121a64b
Sònia Carrancà <sonia.carranca01@estudiant.upf.edu>
2020-06-09 15:02:17 +0200

    Post-seminar

--- Rotation test/Assets/Scripts/collect_food.cs
+++ Rotation test/Assets/Scripts/collect_food.cs
@@ -14,12 +14,12 @@ public class collect_food : MonoBehaviour
         {
             Sound.Instance.PlayCollectedFood();
 
-            coin_prefab.transform.position = player.transform.position + new Vector3(0,0,1000);
+            /*coin_prefab.transform.position = player.transform.position + new Vector3(0,0,1000);
             coin_prefab.GetComponentInChildren<MeshRenderer>().enabled = true;
             coin_prefab.GetComponent<rotate_Coin>().enabled = true;
             coin_prefab.GetComponent<move_coin>().enabled = true;
             coin_prefab.GetComponent<scale_coin>().enabled = true;
-            coin_prefab.GetComponent<destroy_coin>().enabled = true;
+            coin_prefab.GetComponent<destroy_coin>().enabled = true;*/
 
             GameState.Instance.CollectedFood();
             Debug.Log(GameState.Instance.collected_food);
