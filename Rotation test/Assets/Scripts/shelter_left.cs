8f21c245bfa05ea1b3f2d906fe6e04a7fd7a4dfe
GuillemPuente <59957213+GuillemPuente@users.noreply.github.com>
2020-06-15 23:23:49 +0200

    casi que si

---
+++ Rotation test/Assets/Scripts/shelter_left.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class shelter_left : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Menu_Quit"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
